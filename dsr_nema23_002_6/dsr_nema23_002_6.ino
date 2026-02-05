3bc42d49e3cf777daad9b310e91a3a43e1d4f5ff
Madhu Mohan Chary <madhu@marutdrones.com>
2026-02-05 16:30:58 +0530

    Update dsr_nema23_002_6.ino
    Updated 700 steps to 800 steps

--- dsr_nema23_002_6/dsr_nema23_002_6.ino
+++ dsr_nema23_002_6/dsr_nema23_002_6.ino
@@ -66,7 +66,7 @@ volatile int ch10_val = ch10_min; //initial read value
 
 // logic
 bool is_fold = HIGH;//monitor the state of pipes (fold or unfold)
-int steps_to_close_or_open = 700;
+int steps_to_close_or_open = 800;// 700 -> 800
 int dt = 500; //program running delay
 
 void read_pwm()
